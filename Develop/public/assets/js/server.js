cfab886c5048ebf83f3afb621185ade68de28b61
Corey Smith <CSSmith1997@gmail.com>
2020-12-03 17:18:23 -0700

    Added an app.post and an app.delete that posts and removes the different versions of the HTML files.

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -21,3 +21,29 @@ app.get('/api/notes', function (req, res) {
 app.get('*', function (req, res) {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
+
+app.post('/api/notes/:id', function (req, res) {
+    const note = {...req.body, id: uuid() };
+    postNotes(note, function (err, data) {
+        res.json(data);
+    });
+});
+
+app.delete('/api/notes', function (req, res) {
+    deleteNotes(req);
+    res.sendFile(path.join(__dirname, '/public/notes.hmtl'));
+})
+
+function postNotes(cb) {
+    fs.readFileSync(path.join(__dirname, 'db/db.json'), 'utf8', function (err,data) {
+        if (err) throw err;
+        const notes = JSON.parse(data)
+        notes.push(note);
+        fs.writeFile(path.join(__dirname, 'db/db.json'),
+        JSON.stringify(notes),
+        function (err, data) {
+            if (err) throw err;
+            cb(null, notes);
+        })
+    })
+};
