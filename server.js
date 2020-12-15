66d470d5bad2a623c87b4375eb59ab06499a0b91
Corey Smith <CSSmith1997@gmail.com>
2020-12-14 20:59:38 -0700

    Tweaked a couple minor pathing issues again

--- server.js
+++ server.js
@@ -5,25 +5,32 @@ const path = require('path');
 const fs = require('fs');
 const { v4: uuid } = require('uuid');
 
-app.use(express.urlencoded({ extended: true}));
+
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get('/notes', function (req, res){
-    res.sendFile(path.join(__dirname, '/notes.html'));
+
+
+
+// Routes
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
+
 app.get('/api/notes', function (req, res) {
-    getNotes(function (err, data){
+    getNotes(function (err, data) {
         res.json(data);
     });
 });
 
 app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
-app.post('/api/notes/:id', function (req, res) {
-    const note = {...req.body, id: uuid() };
+app.post('/api/notes', function (req, res) {
+    const note = { ...req.body, id: uuid() };
     postNotes(note, function (err, data) {
         res.json(data);
     });
@@ -31,9 +38,11 @@ app.post('/api/notes/:id', function (req, res) {
 
 app.delete('/api/notes/:id', function (req, res) {
     deleteNotes(req);
-    res.sendFile(path.join(__dirname, '/Develop/public/notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 })
 
+
+// Functions
 function getNotes(cb) {
     fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', function (err, data) {
         if (err) throw err;
@@ -41,29 +50,27 @@ function getNotes(cb) {
     })
 };
 
-
 function postNotes(note, cb) {
-    fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8', function (err,data) {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', function (err, data) {
         if (err) throw err;
         const notes = JSON.parse(data)
         notes.push(note);
-        fs.writeFile(path.join(__dirname, '/db/db.json'),
-        JSON.stringify(notes),
-        function (err, data) {
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(notes), function (err, data) {
             if (err) throw err;
             cb(null, notes);
         })
     })
 };
+
 function deleteNotes(req) {
-    fs.readFile(path.join(__dirname, '/db/db.json'), function (err, data) {
+    fs.readFile(path.join(__dirname, '/db/db.json'), 'utf8', function (err, data) {
         if (err) throw err;
-        const notes = JSON.parse(data);
+        const notes = JSON.parse(data)
         const newNotes = notes.filter((item) => item.id != req.params.id);
-        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newNotes), function () {});
+        fs.writeFile(path.join(__dirname, '/db/db.json'), JSON.stringify(newNotes), function () { });
     })
 }
 
 app.listen(PORT, function () {
-    console.log('App is listening on PORT: ' + PORT);
+    console.log('App listening on PORT: ' + PORT);
 });
